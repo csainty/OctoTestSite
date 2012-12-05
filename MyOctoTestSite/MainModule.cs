b3524121d0fb7dee7b8618a1734bc170a8ae5b14
Chris Sainty <csainty@hotmail.com>
2012-12-05 20:08:19 +0100

    Initial commit

---
+++ MyOctoTestSite/MainModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace MyOctoTestSite
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = p => "Hello World";
+        }
+    }
+}
